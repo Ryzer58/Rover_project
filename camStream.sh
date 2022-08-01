7a4e9c3b327a4278b099a754466fa1842f599a57
Ryan L <ryestar101@gmail.com>
2022-08-01 22:19:46 +0100

    fix camera script by remove whitespaces between variables and there assigned values. successfully tested we control interface with img tag method therefore this will now be the go to method as opposed to the old way of using iframes which was less dynamic

--- camStream.sh
+++ camStream.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-PORT = "8080"
-SIZE = "480X360"
-FRAMERATE="10"
+PORT="8080"
+SIZE="640x480"
+FRAMERATE="10" #30 is the max fps for the Trust webcam
 
 export LD_LIBRARY_PATH=/usr/local/LD_LIBRARY_PATH
 mjpg_streamer -i "input_uvc.so -f $FRAMERATE -r $SIZE -d /dev/video1 -y" -o "output_http.so -w /var/www/html -p $PORT"
 
-#For PI video0 should be fine but in the case of the Pcduino video1 tends to the normal source
+#For PI video0 should be work in most case
+#The Pcduino on the otherhand tends to be located at video1 as video is reserved for the hdmi display buffer
