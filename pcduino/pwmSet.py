d526b9ec18196c198dca7021f38c716d5bfc8294
Ryan L <ryestar101@gmail.com>
2022-08-24 18:26:02 +0100

    Additons to documentation in relations to issues discovered with the Pcduino pwm driver. In the case of the Pcduino3 there is now intial support to control a servo through PWM sysfs.

--- pcduino/pwmSet.py
+++ pcduino/pwmSet.py
@@ -30,7 +30,9 @@ def pulseDuration(channel, freq):
     with open(PIN_DUR % id, 'w') as f:
         f.write(str(freq))
 
-def pulseSpeed(channel, value):
+    #Currently broken via sysFs on A10
+
+def pulseDuty(channel, value):
     """set the duty cycle"""
     freq = 0
     id = pwmCheck(channel)
@@ -57,15 +59,43 @@ def polarity(channel, dir):
         f.write('inveresed' if dir == 1 else 'normal')
 
 def servo(channel, angle):
-    """Map an angle to duty Cycle"""
+    """Map the angle to an appropriate duty cycle [works on Pcduino3 only] - Servos typical require a duty cycle of 5 (1.0ms) to 10%(2.0ms), confirm with servo datasheet"""
     id = pwmCheck(channel)
-    value = ((angle/180)*1000000)+1000000 #map out angle to duty cycle
-    dutyCycle = int(value)
+    
+    value = 0(angle/180)*2000000
+    
     #alternative aprouch
-    #angleSpan = 0 - 180
-    #dutySpan = 1000000 - 2000000
-    #scaler = (angle - 0) / 180
-    #value = 1000000 + (scaler * 1000,000)
+    angleSpan = 0 - 180
+    dutySpan = 1000000 - 2000000
+    scaler = (angle - 0) / 180
+    value = 1000000 + (scaler * 1000,000)
+    
+    
+    # duty num | width | Angle - resoltion 9 degrees, possibly 1.8
+    #  2000000    2.0    180
+    #  1950000    1.95   171                
+    #  1900000    1.9    162            
+    #  1850000    1.85   153
+    #  1800000    1.8    144
+    #  1750000    1.75   135
+    #  1700000    1.70   126
+    #  1650000    1.65   117
+    #  1600000    1.6    108
+    #  1550000    1.55    99         
+    #  1500000    1.5     90 
+    #  1450000    1.45    81
+    #  1400000    1.4     72
+    #  1350000    1.35    63
+    #  1300000    1.3     54
+    #  1250000    1.25    45 
+    #  1200000    1.2     36 
+    #  1150000    1.15    27 
+    #  1100000    1.1     18  
+    #  1050000    1.05    9
+    #  1000000    1.0     0
+    dutyCycle = int(value)
     with open(PIN_DUTY_CYCLE % id, 'w') as f:
        f.write(str(dutyCycle))
 
+#def softPwm
+#To implement later
