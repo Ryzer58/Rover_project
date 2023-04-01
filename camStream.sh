3925299094fc6ee9cb5c8773baf8d013027c0f09
Ryan L <ryestar101@gmail.com>
2023-04-01 19:22:15 +0100

    Set out intial blueprint for making the camera control script more flexible

--- camStream.sh
+++ camStream.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# A script to start mjpeg-streamer service, which will need to be called form by refering to it from Rc.lcoal. The primry target  is the USB Trusty webcam so it is not biased to working on
-# the PI only. Depending on the SBC used the video source number seems to change this will need to be kept in mind. I may change over to the Pi Camera when using the PI, so the reference 
-# video source will need to be changed to correspond to the correct video source. Ideally I would like to add some kind of auto detection in future. In relation to running on different SBC's
-# it may be possible to use the internal system files to predict the right video source. It may prove an additional commplication to then employ detect for if the USB camera is plugged in
-# or the PI camera is avaliable given that the initialization will need to be changed from 'input_uvc' to 'raspi_cam'
+# A script to start mjpeg-streamer service. Either the contents should be copied into Rc.lcoal or a link pointing to this file added. To maintain some flexiblity to change between platforms
+# I have included a device option that can simply be changed depending the camera used. In reality this will only make a difference on the PI as the other SBCs do not have a camera connection
+# port. Another thing to consider is that video source differs depending on the SBC used. For some reason this no longer seems to work, need to do a bit more digginh into bash scripts. Ideally
+# once working, try to figure out how to implement somekind of auto detection feature.
 
 PORT="8080"
 SIZE="640x480"
 FRAMERATE="10" #30 is the max fps for the Trust webcam
+DEVICE = "input_uvc.so" #"raspi_cam.so"
+SOURCE = "video1"
 
-export LD_LIBRARY_PATH=/usr/local/LD_LIBRARY_PATH
-mjpg_streamer -i "input_uvc.so -f $FRAMERATE -r $SIZE -d /dev/video1 -y" -o "output_http.so -w /var/www/html -p $PORT"
+
+export LD_LIBRARY_PATH=/home/ryan/mjg-streamer/mjpg-streamer-experiemental
+./mjpg_streamer -i "$DEVICE -f $FRAMERATE -r $SIZE -d /dev/$SOURCE" -o "output_http.so -w ./www -p $PORT"
