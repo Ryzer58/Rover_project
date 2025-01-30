a733d073f44909cc6e6c922c643c7cacebd70fb8
Ryan L <ryestar101@gmail.com>
2025-01-30 23:32:29 +0000

    Redraft notes for improved clarity and remove references for input_raspicam as the dependencies have been completed removed in the latest Raspbian OS

--- camStream.sh
+++ camStream.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# A script to start mjpeg-streamer service. Either the contents should be copied into Rc.lcoal or a link pointing to this file added. To maintain some flexiblity to change between platforms
-# I have included a variable for setting the input device. At some point I would like to make this condition based where by examine something like the OS release can be used to predicted
-# what port would be used. In addtion there is a device variable which is mainly for use on the PI as other Hardware I current have does not have a readily available camera port. 
+# A script to start mjpeg-streamer service. Either the contents can be copied into rc.local directly or 
+# invoke this script from rc.local. Ideally this should be universal however different hardware have
+# differing video devices meaning that we would need a mechanism to identify the board and then if the
+# expected device is avaliable. This is complicated on platforms like the Raspberry Pi where the camera
+# can be mounted to the CSI or USB port. Other SBCs like the Beaglebone Ai can only have a USB camera
+# connection. 
 
 PORT="8080"
 SIZE="640x480"
-FRAMERATE="10" #30 is the max fps for the Trust webcam
+FRAMERATE="10" # Adjust according to capabilites of the camera/CPU utilization (Trust webcam max = 30)
 DEVICE="input_uvc.so" 
-# DEVICE="input_raspicam.so" #may not longer be suppported due switch over to libcam on newer kernels
-SOURCE="video1" #"video0"
+SOURCE="video1" # for now manual adjust for hardware used
 
 
 export LD_LIBRARY_PATH=/usr/local/lib
 mjpg_streamer -i "$DEVICE -f $FRAMERATE -r $SIZE -d /dev/$SOURCE" -o "output_http.so -w /usr/local/share/mjpg-streamer/www -p $PORT"
 
-# Not all options supported are the same so slightly different parameters need to be passed as well, refer to documentation for more detail
-# mjpg_streamer -i "$DEVICE -fps $FRAMERATE -r $SIZE -d /dev/$SOURCE" -o "output_http.so -w /usr/local/share/mjpg-streamer/www -p $PORT"
 
 exit
